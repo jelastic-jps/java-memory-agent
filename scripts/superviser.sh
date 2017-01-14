83cd997c8920ba49936f2a95be8e2bf8cc3e1513
sych74 <sych74@gmail.com>
2017-01-14 22:40:42 +0200

    Update superviser.sh

--- scripts/superviser.sh
+++ scripts/superviser.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 
+# Absolute path to this script, e.g. /home/user/bin/foo.sh
+SCRIPT=$(readlink -f "$0")
+# Absolute path this script is in, thus /home/user/bin
+SCRIPT_PATH=$(dirname "$SCRIPT")
+
 [[ "$1" == "--install" ]] && {
    JAVA_ORIG=$(which java)
-   echo "JAVA_ORIG=$JAVA_ORIG" >> /etc/environment
-   
-   # Absolute path to this script, e.g. /home/user/bin/foo.sh
-   SCRIPT=$(readlink -f "$0")
-   # Absolute path this script is in, thus /home/user/bin
-   SCRIPT_PATH=$(dirname "$SCRIPT")
+   echo "JAVA_ORIG=$JAVA_ORIG" >> /etc/environment   
    echo "PATH=$SCRIPT_PATH:\$PATH" >> /etc/environment
-  
+ 
    mv $SCRIPT "$SCRIPT_PATH/java"
    chmod $( stat -f '%p' ${JAVA_ORIG} ) "$SCRIPT_PATH/java"
 }
+
+source "$SCRIPT_PATH/memoryConfig.sh";
 
 $JAVA_ORIG "$@"
