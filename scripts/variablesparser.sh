aea86e12b8524abeaf6653fc7b0d1857f28a4833
Dimka Tsurko <dt@jelastic.com>
2018-02-13 14:57:29 +0200

    JE-36748

--- scripts/variablesparser.sh
+++ scripts/variablesparser.sh
@@ -4,13 +4,16 @@
 }
 
 CONFFILE="${JAVA_OPTS_CONFFILE}"
-confresult=""
+declare -a confresult=()
 
 if [ -f "$CONFFILE" ]; then
-    confdata=$(sed '/^#/d' $CONFFILE | awk -F# '{print $1}' | sed 's/\s/\n/g')
-    for i in $confdata
+	OLD_IFS="$IFS"
+	IFS=$'\n'
+	val=($(cat "$CONFFILE" | grep -v '#' | grep -v -vE '^\s*$' | sed -re "s/('|\") /'\n/g" -e "/(\"|')/! s/\s/\n/g" -e 's/(\s)(-.*=\")/\n\2/'))
+	IFS="$OLD_IFS"
+
+	for i in "${val[@]}"
     do
-        if $(echo $i | grep -q '#'); then continue; fi
         if $(grep -qiE '\-Xmx[[:digit:]]{1,}[mgkMGK]$' <<< $i); then XMX="${i}";
         elif $(grep -qiE '\-Xms[[:digit:]]{1,}[mgkMGK]$' <<< $i); then XMS="${i}";
         elif $(grep -qiE '\-Xmn[[:digit:]]{1,}[mgkMGK]$' <<< $i); then XMN="${i}";
@@ -18,9 +21,10 @@ if [ -f "$CONFFILE" ]; then
         elif $(grep -qiE '\-Xmaxf[[:digit:]\.]{1,}$' <<< $i); then XMAXF="${i}";
         elif $(grep -qiE '\-XX:MaxPermSize=[[:digit:]\.]{1,}[mgkMGK]$' <<<  $i); then MAXPERMSIZE="${i}";
         else
-            confresult=" $confresult $i"
+			confresult=("${confresult[@]}" "$i")
         fi
     done
+
 fi
 
 [ -z "$XMX" ] || export XMX
@@ -29,4 +33,4 @@ fi
 [ -z "$XMINF" ] || export XMINF
 [ -z "$XMAXF" ] || export XMAXF
 [ -z "$MAXPERMSIZE" ] || export MAXPERMSIZE
-[ -z "$confresult" ] || set -- $confresult "$@"
+[ -z "$confresult" ] || set -- "${confresult[@]}" "${@}"
